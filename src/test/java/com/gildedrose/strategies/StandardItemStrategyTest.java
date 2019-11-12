cd25400316f6a9cd4c951188cec38a9d7d2fd2f2
Mateusz Soroka <xmsoroka@gmail.com>
2019-11-12 18:27:44 +0100

    Separate strategies test

---
+++ src/test/java/com/gildedrose/strategies/StandardItemStrategyTest.java
@@ -0,0 +1,47 @@
+package com.gildedrose.strategies;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.*;
+
+import com.gildedrose.Item;
+import com.gildedrose.enums.ItemCategory;
+
+import org.junit.Test;
+
+public class StandardItemStrategyTest {
+    private static final String NAME = "Standard Item";
+    private static final int SELL_IN = 10;
+    private static final int QUALITY = 20;
+    private static final ItemCategory CATEGORY = ItemCategory.Standard;
+    private StandardItemStrategy standardStrategy = new StandardItemStrategy();
+
+    @Test
+    public void decreaseSellInAndQuality() {
+        Item item = new Item(NAME, SELL_IN, QUALITY, CATEGORY);
+
+        item = this.standardStrategy.updateItem(item);
+
+        assertEquals(SELL_IN - 1, item.getSellIn());
+        assertEquals(QUALITY - 1, item.getQuality());
+    }
+
+    @Test
+    public void decreaseQualityTwiceAsNormal() {
+        Item item = new Item(NAME, 0, QUALITY, CATEGORY);
+
+        item = this.standardStrategy.updateItem(item);
+
+        assertEquals(0, item.getSellIn());
+        assertEquals(QUALITY - 2, item.getQuality());
+    }
+
+    @Test
+    public void sellInAndQualityNotNegative() {
+        Item item = new Item(NAME, 0, 0, CATEGORY);
+
+        item = this.standardStrategy.updateItem(item);
+
+        assertEquals(0, item.getSellIn());
+        assertEquals(0, item.getQuality());
+    }
+}

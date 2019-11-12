83ce015ec901b5b600c4368a33941871d481b921
Mateusz Soroka <xmsoroka@gmail.com>
2019-11-12 21:11:11 +0100

    Backstage Pass item refactor

--- src/main/java/com/gildedrose/strategies/BackstagePassItemStrategy.java
+++ src/main/java/com/gildedrose/strategies/BackstagePassItemStrategy.java
@@ -7,22 +7,18 @@ public class BackstagePassItemStrategy implements ItemStrategy {
     @Override
     public Item updateItem(Item item) {
         int newSellIn = this.isSellInPositive(item.getSellIn()) ? item.getSellIn() - 1 : 0;
-        int newQuality = 0;
+        int newQuality = this.isSellInMoreThanTen(newSellIn) ? item.getQuality() + 1 : 0;
 
         if (this.isSellInFiveOrLess(newSellIn)) {
             newQuality += item.getQuality() + 3;
-        } else if (this.isSellInTenOrLess(newSellIn)) {
+        }
+
+        if (this.isSellInTenOrLess(newSellIn)) {
             newQuality += item.getQuality() + 2;
-        } else {
-            newQuality += item.getQuality() + 1;
         }
 
         if (this.isQualityMoreThanFifty(newQuality)) {
             newQuality = 50;
-        }
-
-        if (!this.isSellInPositive(newSellIn)) {
-            newQuality = 0;
         }
 
         item.setSellIn(newSellIn);
@@ -31,12 +27,16 @@ public class BackstagePassItemStrategy implements ItemStrategy {
         return item;
     }
 
+    private boolean isSellInMoreThanTen(int sellIn) {
+        return sellIn > 10;
+    }
+
     private boolean isSellInFiveOrLess(int sellIn) {
-        return sellIn <= 5;
+        return sellIn > 0 && sellIn <= 5;
     }
 
     private boolean isSellInTenOrLess(int sellIn) {
-        return sellIn <= 10;
+        return sellIn > 5 && sellIn <= 10;
     }
 
     private boolean isSellInPositive(int sellIn) {
